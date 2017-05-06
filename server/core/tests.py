d01c76f4adba6cc44abb3bd3f0dd0b4db7525d04
Ray Chen <ray@half0wl.com>
2017-05-06 14:31:18 +0800

    User and UserProfile tests

--- server/core/tests.py
+++ server/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from core.models import UserProfile, Project
 
-# Create your tests here.
+
+class UserTest(TestCase):
+
+    def test_will_create_user_profile(self):
+        test_user = User(username='test', email='t@t.co', password='hunter2')
+        test_user.save()
+        test_user_profile = UserProfile.objects.get(user_id=test_user.id)
+
+        self.assertEqual(test_user.id, test_user_profile.user_id)
+        self.assertEqual(test_user_profile.bio, 'Hello!')
