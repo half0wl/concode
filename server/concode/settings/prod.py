48f454713f674f0723ea25b2f25fceca30ad24c6
Ray Chen <ray@half0wl.com>
2017-05-15 21:20:03 +0800

    Turning on debug to debug 500 error

--- server/concode/settings/prod.py
+++ server/concode/settings/prod.py
@@ -4,7 +4,7 @@ import dj_database_url
 from concode.settings.base import *
 
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 ALLOWED_HOSTS = ['concode-api.herokuapp.com']
 db_from_env = dj_database_url.config(conn_max_age=500)
